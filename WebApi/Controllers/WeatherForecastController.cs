d586c956b6beb8a1cf4b8db7f63c05ecf8510e28
CHEJARA Vijendra <vijendra.chejara@sodexo.com>
2021-08-07 13:05:45 +0530

    Updated Route name

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace WebApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("forecast")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
